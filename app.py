6efd898817b284c4f213f06d1b1c3a112631063c
xdapperdonx <xdapperdonx@Viserion>
2024-03-24 23:25:05 -0500

    multiple files

--- app.py
+++ app.py
@@ -1,53 +1,72 @@
 import os
 import pyheif
+import zipfile
 from PIL import Image
 from io import BytesIO
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, jsonify
 
 app = Flask(__name__)
 
-def heic_to_jpeg(heic_file):
-    try:
-        #reads in heic file
-        heif_file = pyheif.read(heic_file)
+def heic_to_jpeg(upload_files):
+    converted_files_list = []
+    for file in upload_files:
+        try:
+            new_filename = os.path.splitext(os.path.basename(file.filename))[0] + ".jpeg"
 
-        #create image object from heic file
-        image = Image.frombytes(
-            heif_file.mode, 
-            heif_file.size, 
-            heif_file.data,
-            "raw",
-            heif_file.mode,
-            heif_file.stride,
-        )
+            try:
+                #reads in heic file
+                heif_file = pyheif.read(file)
+            except ValueError:
+                print(file.filename)
 
-        return image
 
-    except Exception as e:
-        print(f"{e} file: {heic_file}")
+            #create image object from heic file
+            image = Image.frombytes(
+                heif_file.mode, 
+                heif_file.size, 
+                heif_file.data,
+                "raw",
+                heif_file.mode,
+                heif_file.stride,
+            )
+
+            img_io = BytesIO()
+            img_io.name = new_filename
+            image.save(img_io, 'JPEG')
+            img_io.seek(0)            
+
+            converted_files_list.append(img_io)
+    
+        except Exception as e:
+            print(f"{e} file: {heic_file}")
+
+    return converted_files_list
 
 @app.route('/', methods=["GET","POST"])
-def upload_file():
+def upload():
+    upload_files = request.files.getlist('files')
+
     if request.method == "POST":
-        if 'file' not in request.files:
-            return 'No file uploaded', 400
-        
-        file = request.files['file']
-        if file.filename == '':
-            return 'No file selected', 400
-        
-        if file:
-            output_file = heic_to_jpeg(file)
-           
-            img_io = BytesIO()
-            output_file.save(img_io, 'JPEG')
-            img_io.seek(0)
+        if len(upload_files) <= 0 or upload_files[0].filename == "":
+            return 'No files uploaded', 400
+        else:
+            files_list = heic_to_jpeg(upload_files)
+            zip_buffer = BytesIO()
 
-            filename_without_extension = os.path.splitext(os.path.basename(file.filename))[0]
+            temp_dir = 'temp'
+            os.makedirs(temp_dir, exist_ok=True)
 
-            return send_file(img_io, mimetype = "image/jpeg", as_attachment = True, download_name = f"{filename_without_extension}.jpeg")
-        
+            zip_file_path = os.path.join(temp_dir, "files.zip")
+            with zipfile.ZipFile(zip_file_path, 'w') as zipf:
+                for index, image in enumerate(files_list, start=0):
+                    image_name = f'image{index}.jpg'
+                    zipf.writestr(image_name, image.getvalue())
+
+            zip_buffer.seek(0)
+
+            return send_file(zip_file_path, as_attachment = True, download_name = "images.zip")
+       
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug = True, port = 5100)
+    app.run(host = "0.0.0.0", debug = True, port = 5100)
